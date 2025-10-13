c0c65885116f5bc9e4fb6fa7c1dba379b99839e6
Xuwei <qinxuwei15@gmail.com>
2025-10-13 18:41:17 +0200

    Initial project including create entities

---
+++ src/main/java/com/xuwei/prostore/ProstoreApplication.java
@@ -0,0 +1,13 @@
+package com.xuwei.prostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xuwei/prostore/ProstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuwei.prostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
